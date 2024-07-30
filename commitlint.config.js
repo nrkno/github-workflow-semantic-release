877613f7e51fa69ca4f43382cae6d7c8e8437707
André Lersveen <7195448+lersveen@users.noreply.github.com>
2024-07-30 09:27:11 +0200

    fix: disable subject case rule
    Disable strict subject casing to be closer to the official conventional commits specification.

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [2, 'always', 256],
-    'footer-max-line-length': [2, 'always', 256]
+    'footer-max-line-length': [2, 'always', 256],
+    'subject-case': [0, 'always', 'lower-case']  // Disable subject case rule
   },
 }
