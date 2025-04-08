7c8084047d8b0515093095b0b41af55d4404afbe
Martin Bruset Solberg <martin.bruset.solberg@nrk.no>
2025-04-08 12:38:58 +0200

    fix: format commitling.config to mjs

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [2, 'always', 256],
@@ -6,4 +6,4 @@ module.exports = {
     'subject-max-length': [0],
     'subject-case': [0, 'always', 'lower-case']  // Disable subject case rule
   },
-}
+};
