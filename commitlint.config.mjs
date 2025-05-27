96a04b12465f4702a7a655aebc8ef0c68b8c8a18
Johan Berg <joberget@users.noreply.github.com>
2025-05-27 09:13:35 +0200

    fix: set header max length to 200

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -4,6 +4,7 @@ export default {
     'body-max-line-length': [2, 'always', 256],
     'footer-max-line-length': [2, 'always', 256],
     'subject-max-length': [0],
-    'subject-case': [0, 'always', 'lower-case']  // Disable subject case rule
+    'subject-case': [0, 'always', 'lower-case'],  // Disable subject case rule
+    'header-max-length' : [2, 'always', 200]
   },
 };
